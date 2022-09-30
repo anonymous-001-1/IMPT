b6de220627eb53add524b190168583af0d251e5f
hoysal <surajhoysal817@gmail.com>
2022-09-30 08:41:05 +0530

    IMPL functions tested

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-network-helpers")
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.12",
+};
